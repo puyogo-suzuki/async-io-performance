c6e1ecd6334760fe6b6d311dd307a5f3284127be
Go Suzuki <puyogo.suzuki@gmail.com>
2021-09-24 18:09:27 +0900

    [CHANGE] unit of file size is kiB.

--- initfile.c
+++ initfile.c
@@ -11,7 +11,7 @@ int main(int argc, char * argv[]) {
         printf("Usage: %s <file size> <file name>", argv[0]);
         return 0;
     }
-    int size = atoi(argv[1]);
+    int size = atoi(argv[1]) * 1024;
     int fd = creat(argv[2], 0777);
     char buf[1024];
     int i = 0;
@@ -19,12 +19,6 @@ int main(int argc, char * argv[]) {
         for(int j = 0; j < 1024; j += sizeof(int))
             *(int *)(&buf[j]) = rand();
         write(fd, buf, sizeof(buf));
-    }
-    if(i - size > 0) {
-        int n = 1024 - (i - size);
-        for(int j = 0; j < 1024; j += sizeof(int))
-            *(int *)(&buf[j]) = rand();
-        write(fd, buf, sizeof(char) * n);
     }
     close(fd);
     return 0;
